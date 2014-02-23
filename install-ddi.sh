85d787d2287b0b087a154300306cea542895ab59
Rorist <aubort.jeanbaptiste@gmail.com>
2014-02-23 21:08:04 +0100

    Run emul before

--- install-ddi.sh
+++ install-ddi.sh
@@ -5,6 +5,14 @@ wget http://dl.google.com/android/ndk/android-ndk-r9c-linux-x86.tar.bz2
 tar xvf android-ndk-r9c-linux-x86.tar.bz2
 echo 'PATH=$PATH:~/android-ndk-r9c/' >> .bashrc
 
+# Run emulator
+emulator -avd Android-2.3&
+echo Starting emulator .
+for i in $(seq 30); do
+    sleep 1
+    echo .
+done
+
 # Install ADBI
 git clone https://github.com/crmulliner/adbi.git
 cd adbi/hijack/jni/
@@ -13,14 +21,6 @@ cd ..
 adb push libs/armeabi/hijack /data/local/tmp/
 cd ~/adbi/instruments/base/jni
 ndk-build
-
-# Run emulator
-emulator -avd Android-2.3&
-echo Starting emulator .
-for i in $(seq 30); do
-    sleep 1
-    echo .
-done
 
 # Install DDI
 cd
